f2ede08725f8fb3133e3ce81ac19e0a800baa507
LyricsON <lyricson0203@gmail.com>
2023-05-25 00:11:43 +0100

    first commit

---
+++ src/Component/profile/Address.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Address() {
+  return (
+    <div>
+      <p>Cité Habib</p>
+    </div>
+  );
+}
+
+export default Address;
---
+++ src/Component/profile/ProfilePhoto.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ProfilePhoto() {
+  return (
+    <div>
+      <img
+        src="https://scontent.cdninstagram.com/v/t51.2885-19/344555381_549262797399982_471199601846557651_n.jpg?stp=dst-jpg_s200x200&_nc_cat=108&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=aiX8Pj4snhcAX_1riwv&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AfAlv2DWJibCcZlnC-CSoxBYdhDmpXS-4rU3zvHiaB8aAw&oe=6495E7EC"
+        alt="teswira"
+      />
+    </div>
+  );
+}
+
+export default ProfilePhoto;
